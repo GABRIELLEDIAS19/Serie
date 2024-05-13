117fccea5bdb2aa03cd0b40be243abc67d24840e
Gabrielle Dias <169068323+GABRIELLEDIAS19@users.noreply.github.com>
2024-05-13 19:41:01 +0000

    artista serie

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,14 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("################");
+    System.out.println("série:Outer Banks");
+    System.out.println("################");
+    System.out.println("####");
+    System.out.println("Em uma ilha norte-americana onde os moradores rivalizam entre si, principalmente pela diferença de classes sociais do local, o jovem Jonh B convoca três amigos, JJ, Kiara e Pope para procurarem um tesouro lendário ligado ao desaparecimento de seu pai.");
+    System.out.println("####");
+    System.out.println("https://youtu.be/pfY3j-3uQhk?si=0xS0alzPhKMPUROb");
+    
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+  
 }
